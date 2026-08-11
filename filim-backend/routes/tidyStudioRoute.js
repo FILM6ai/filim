e4627e213717f03bb642623ba0b5ed84e195a523
anirudhatalmale6-alt <anirudhatalmale6-alt@users.noreply.github.com>
2026-08-11 10:47:05 +0000

    Remove temporary studio bgImage tidy endpoint

--- filim-backend/routes/tidyStudioRoute.js
+++
@@ -1,8 +0,0 @@
-// TEMPORARY route for the one-off studio bgImage cleanup. Remove after use.
-import express from "express";
-import { tidyStudio } from "../controllers/tidyStudioController.js";
-
-const tidyStudioRouter = express.Router();
-tidyStudioRouter.get("/studio-bgimages", tidyStudio);
-
-export default tidyStudioRouter;
